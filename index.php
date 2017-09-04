1e0653986fa0021d5d67f71e66abbd0470bc1905
Akoni <hakmorrison@csumb.edu>
2017-09-04 10:16:07 +0000

    updated pages

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/ >
+        <title>Homework 1</title>
+        <link rel="stylesheet" href="styles.css" type="text/css" />
+    </head>
+    <body>
+        <h1>Homework 1</h1>
+        
+        <nav>
+            <a href="cs1.html">CS Topic 1</a>  <br>
+            <a href="cs2.html">CS Topic 2</a>  <br>
+            <a href="cs3.html">CS Topic 3</a>  <br>
+            <a href="cs4.html">CS Topic 4</a>  <br>
+        </nav>
+    </body>
+</html>
