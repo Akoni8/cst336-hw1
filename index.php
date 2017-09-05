6eb00f008fa816f975314f90857e0e59a7a1a708
Akoni <hakmorrison@csumb.edu>
2017-09-05 21:47:01 +0000

    changed image sizes, added text

--- index.php
+++ index.php
@@ -6,23 +6,24 @@
         <link rel="stylesheet" href="styles.css" type="text/css" />
     </head>
     <body>
+        
+        <nav>
+            <a href="index.php">Home</a> 
+            <a href="cs1.html">Python</a>  
+            <a href="cs2.html">Java</a>  
+            <a href="cs3.html">Swift</a> 
+        </nav>
+        
         <h1>Homework 1</h1>
         <h2>Programming Languages</h2>
         
         <img src="programlanguage.jpg"></img>
-        
-        
-        <nav>
-            <a href="index.php">Home</a> <br>
-            <a href="cs1.html">Python</a>  <br>
-            <a href="cs2.html">Java</a>  <br>
-            <a href="cs3.html">Swift</a>  <br>
-        </nav>
         
         <div>
             <p>Programming languages are formal languages that follow specific sets of instructions used to produce various kinds of outputs.
             They generally consist of instructions for a computer and can be used to create programs by implementing specific algorithms.
             </p>
         </div>
+        
     </body>
 </html>
