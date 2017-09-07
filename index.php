8cf484bd4aaf044e9ed2fe153418009b150b81ac
Akoni <hakmorrison@csumb.edu>
2017-09-07 03:02:04 +0000

    finished hw

--- index.php
+++ index.php
@@ -14,18 +14,31 @@
             <a href="cs3.html">Swift</a> 
         </nav>
         
-        <h1>Homework 1</h1>
+        <h1 class= "heading" id="homework">Homework 1</h1>
         <h2>Programming Languages</h2>
         
-        <img src="programlanguage.jpg"></img>
+        <figure>
+            <img src="programlanguage.jpg"></img>
+        </figure>
         
         <div id = "container">
-            <p>Programming languages are formal languages that follow specific sets of instructions used to produce various kinds of outputs.
+            <p class="info">Programming languages are formal languages that follow specific sets of instructions used to produce various kinds of outputs.
             They generally consist of instructions for a computer and can be used to create programs by implementing specific algorithms.
             </p>
+            
+            <a href ="https://en.wikipedia.org/wiki/Programming_language">Programming-Language-Wikipedia</a>
+            
+            <ul>
+                <li>Python</li>
+                <li>Java</li>
+                <li>Swift</li>
+            </ul>
+             <iframe width="560" height="315" src="https://www.youtube.com/embed/RU1u-js7db8" frameborder="0" allowfullscreen></iframe> 
         </div>
          
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/RU1u-js7db8" frameborder="0" allowfullscreen></iframe> 
+       
+        
+        
         
     </body>
 </html>
